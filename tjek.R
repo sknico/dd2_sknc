39440f1f152c383ac3d0fc1734ce971166158aa1
Sia Kromann Nicolaisen <sknc@clin.au.dk>
2024-11-12 07:41:18 +0100

    lille ændring

--- tjek.R
+++ tjek.R
@@ -1,11 +1,4 @@
 # Små ændringer til at tjekke om det viker
-
-
-# fil til at tjekke github
+# endnu en lille ændring
 
 getwd()
-
-git remote set-url origin git@github.com:sknico/dd2_sknc.git
-
-
-git push origin HEAD:refs/heads/main

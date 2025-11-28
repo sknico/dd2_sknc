af2c53249d86a49f30844adeb2118f556e2272c9
SKNC <sknc@clin.au.dk>
2025-11-28 11:58:58 +0100

    abbr problems

--- _extensions/abbr_auto/abbr_auto.lua
+++ _extensions/abbr_auto/abbr_auto.lua
@@ -1,8 +1,11 @@
 
--- Read abbreviations.yml using Pandoc's YAML parser
 function read_abbreviations()
-  local file = io.open("abbreviations.yml", "r")
-  if not file then return {} end
+  local path = "abbreviations.yml"
+  local file = io.open(path, "r")
+  if not file then
+    print("Could not find abbreviations.yml at: " .. path)
+    return {}
+  end
   local content = file:read("*a")
   file:close()
   local doc = pandoc.read("---\n" .. content .. "\n---", "markdown")
@@ -10,23 +13,19 @@ function read_abbreviations()
 end
 
 local abbreviations = read_abbreviations()
-local seen = {} -- Track which abbreviations have been expanded
+local seen = {}
 
 function Str(el)
   local text = el.text
-  -- Remove punctuation from start and end (commas, periods, colons, semicolons, etc.)
   local clean_text = text:gsub("^[%p]+", ""):gsub("[%p]+$", "")
-
   if abbreviations[clean_text] then
     local full = pandoc.utils.stringify(abbreviations[clean_text])
     if not seen[clean_text] then
-      -- First occurrence: full term + abbreviation with <abbr>
       seen[clean_text] = true
       return pandoc.RawInline("html",
         full .. " (<abbr title=\"" .. full .. "\">" .. clean_text .. "</abbr>)"
       )
     else
-      -- Subsequent occurrences: just <abbr>
       return pandoc.RawInline("html",
         "<abbr title=\"" .. full .. "\">" .. clean_text .. "</abbr>"
       )

13c57560549d9b0fc404bdc18381e4d3b85dd184
SKNC <sknc@clin.au.dk>
2025-11-28 07:37:40 +0100

    abbr 2

--- _extensions/abbr/abbr.lua
+++ _extensions/abbr/abbr.lua
@@ -2,7 +2,7 @@ function Span(el)
   if el.classes:includes("abbr") then
     local title = el.attributes["title"]
     if title then
-      return pandoc.RawInline("html", "<abbr title=\"" .. title .. "\">" .. pandoc.utils.stringify(el.content) .. "</abbr>")
+      return pandoc.RawInline("html", "<abbr title="" .. title .. "">" .. pandoc.utils.stringify(el.content) .. "</abbr>")
     end
   end
 end

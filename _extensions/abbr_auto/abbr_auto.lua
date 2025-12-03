75060d7cf8494510478bd1331df93f5f482094f0
SKNC <sknc@clin.au.dk>
2025-12-03 13:42:48 +0100

    nu virker abbr igen (men jeg mangler stadig at lave ændringer)

--- _extensions/abbr_auto/abbr_auto.lua
+++ _extensions/abbr_auto/abbr_auto.lua
@@ -1,24 +1,10 @@
-
 function read_abbreviations()
-  -- Get the project root from PANDOC_STATE or fallback to current working directory
-  local root = PANDOC_STATE.input_files[1] and pandoc.path.directory(PANDOC_STATE.input_files[1]) or "."
-  
-  -- Move up until we find abbreviations.yml
-  local path = pandoc.path.join({root, "abbreviations.yml"})
+  local path = "abbreviations.yml"
   local file = io.open(path, "r")
-
-  -- If not found, try walking up directories
-  while not file and root ~= "." do
-    root = pandoc.path.directory(root)
-    path = pandoc.path.join({root, "abbreviations.yml"})
-    file = io.open(path, "r")
-  end
-
   if not file then
-    print("Could not find abbreviations.yml")
+    print("Could not find abbreviations.yml at: " .. path)
     return {}
   end
-
   local content = file:read("*a")
   file:close()
   local doc = pandoc.read("---\n" .. content .. "\n---", "markdown")

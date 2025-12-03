9c74eea6f577b0e3d086ab490b6692dda52a6b46
SKNC <sknc@clin.au.dk>
2025-12-03 13:39:17 +0100

    fjern abb (der er noget der ikke virker)

--- _extensions/abbr_auto/abbr_auto.lua
+++ _extensions/abbr_auto/abbr_auto.lua
@@ -1,11 +1,24 @@
 
 function read_abbreviations()
-  local path = "abbreviations.yml"
+  -- Get the project root from PANDOC_STATE or fallback to current working directory
+  local root = PANDOC_STATE.input_files[1] and pandoc.path.directory(PANDOC_STATE.input_files[1]) or "."
+  
+  -- Move up until we find abbreviations.yml
+  local path = pandoc.path.join({root, "abbreviations.yml"})
   local file = io.open(path, "r")
+
+  -- If not found, try walking up directories
+  while not file and root ~= "." do
+    root = pandoc.path.directory(root)
+    path = pandoc.path.join({root, "abbreviations.yml"})
+    file = io.open(path, "r")
+  end
+
   if not file then
-    print("Could not find abbreviations.yml at: " .. path)
+    print("Could not find abbreviations.yml")
     return {}
   end
+
   local content = file:read("*a")
   file:close()
   local doc = pandoc.read("---\n" .. content .. "\n---", "markdown")

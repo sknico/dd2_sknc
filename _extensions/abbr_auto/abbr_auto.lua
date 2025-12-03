f723a1fb48464953c02c651f091fa93e4e784830
SKNC <sknc@clin.au.dk>
2025-12-03 14:42:59 +0100

    abbr global

--- _extensions/abbr_auto/abbr_auto.lua
+++ _extensions/abbr_auto/abbr_auto.lua
@@ -1,5 +1,5 @@
 
--- Always load abbreviations.yml from project root
+-- Load abbreviations.yml from project root
 function read_abbreviations()
   local root = os.getenv("QUARTO_PROJECT_DIR") or "."
   local path = root .. "/abbreviations.yml"
@@ -18,8 +18,8 @@ function read_abbreviations()
   return doc.meta
 end
 
+-- Load abbreviations
 local abbreviations = read_abbreviations()
-local seen = {}
 
 -- Normalize keys for case-insensitive lookup
 local normalized_abbr = {}
@@ -27,6 +27,29 @@ for k, v in pairs(abbreviations) do
   normalized_abbr[k:lower()] = pandoc.utils.stringify(v)
 end
 
+-- Persistent state file for seen abbreviations
+local state_file = (os.getenv("QUARTO_PROJECT_DIR") or ".") .. "/docs/.abbr_seen"
+
+-- Load seen abbreviations from file
+local seen = {}
+local f = io.open(state_file, "r")
+if f then
+  for line in f:lines() do
+    seen[line] = true
+  end
+  f:close()
+end
+
+-- Append new seen abbreviation to file
+local function save_seen(key)
+  local f = io.open(state_file, "a")
+  if f then
+    f:write(key .. "\n")
+    f:close()
+  end
+end
+
+-- Pandoc filter: replace abbreviations
 function Str(el)
   local text = el.text
   -- Remove leading/trailing punctuation for clean match
@@ -35,8 +58,13 @@ function Str(el)
 
   if normalized_abbr[key] then
     local full = normalized_abbr[key]
+
+    -- Debug log
+    io.stderr:write("[abbr_auto] Found abbreviation: " .. clean_text .. "\n")
+
     if not seen[key] then
       seen[key] = true
+      save_seen(key)
       return pandoc.RawInline("html",
         full .. " (<abbr title=\"" .. full .. "\">" .. clean_text .. "</abbr>)"
       )
